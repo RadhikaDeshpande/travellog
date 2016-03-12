c51f180eadfd30cbcdba228743085e41e2b93b12
RadhikaDeshpande <radhikadeshpande21@gmail.com>
2016-03-11 18:53:10 -0800

    CSS handling for view blogs by location

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -55,9 +55,9 @@ $(document).ready(function(){
 
 	$('#viewblogsbylocation').click(function() {
 		//http://localhost:3000/apis/blogs/?q_case=2&location_name= 
-    alert("Location is hardcoded for now for this query");
+    // alert("Location is hardcoded for now for this query");
 		$.ajax({
-			url: '/apis/blogs?q_case=2&location_name=San Francisco',
+			url: '/apis/blogs?q_case=2&location_name=Chicago',
 			type: 'GET',
 			data: "",
 			async: false,
@@ -65,8 +65,28 @@ $(document).ready(function(){
 			contentType: false,
 			processData: false,
 			success: function (returndata) {
-				alert("Inside Ajax success of viewblogsbylocation - display the returndata");
-			  //alert(returndata);
+				// alert("Inside Ajax success of viewblogsbylocation - display the returndata");
+			  // alert(returndata);
+			  // alert(returndata.length);
+			  var obj = JSON.parse(returndata);
+			  // $.each(obj, function() {
+		   //      alert(this);             
+		   //  })
+		   var blog_location = obj['_id'];
+		   var blog_count = obj['totalBlogCount'];
+		   var blogs = obj['blogs'];
+		   $.each(blogs, function() {
+		        // alert(this['content']);
+		        // alert(this['images']);
+		    });
+
+			  $("#blogcontent").append("<div><h3> Blogs in "+ blog_location + "</h3></div>");
+			  for (var i = 0; i < blog_count; i++) {
+			  	var contentDivId = "contentDiv" + i;
+			  	var imageSrcId = "imageSrc" + i;
+			  	$("#blogcontent").append("<div class=\"contentDivClass\" id=" + contentDivId + ">" + blogs[i]['content'] + "</div>");
+			  	$("#blogcontent").append("<img class=\"imageSrcClass\" id=" + imageSrcId + " src=" + blogs[i]['images'][0] + "></img>");
+			  }
 			}
 		});
 
